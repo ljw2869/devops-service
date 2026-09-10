f39347e6e2761eb98a322f02b5dcaaccd88aad7b
jiwonlee <ljw2869@hanmail.net>
2026-09-10 15:53:43 +0900

    first

---
+++ src/main/java/com/example/devopsservice/AppController.java
@@ -0,0 +1,15 @@
+package com.example.devopsservice;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController 
+public class AppController{
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+    
+}
