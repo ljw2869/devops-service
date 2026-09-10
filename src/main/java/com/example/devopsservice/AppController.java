b41b9e392cb36dcc832ac7b6782acbdd2cf875de
jiwonlee <ljw2869@hanmail.net>
2026-09-10 16:35:47 +0900

    first ci/cd

--- src/main/java/com/example/devopsservice/AppController.java
+++ src/main/java/com/example/devopsservice/AppController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AppController{
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "Good Day!!";
     }
     
 }
